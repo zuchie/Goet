a721d87db96e702d9faca02dd19e9318856b9928
zuchie <zuchie@gmail.com>
2017-06-08 03:53:44 -0700

    Add Email feature on About page.

--- Goet/AboutViewController.swift
+++ Goet/AboutViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     @IBOutlet weak var text: UITextView!
     private var textTitle: String!
@@ -17,8 +18,17 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if textTitle != "About the Goet App" {
+            navigationItem.rightBarButtonItem = nil
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         navigationItem.title = textTitle
-        text.text = textContent
+        
+        DispatchQueue.main.async {
+            self.text.text = self.textContent
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,6 +36,38 @@ class AboutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func handleEmailTap(_ sender: UIBarButtonItem) {
+        let emailComposeVC = configureEmailComposeVC()
+        if MFMailComposeViewController.canSendMail() {
+            present(emailComposeVC, animated: true, completion: nil)
+        } else {
+            showSendEmailErrorAlert()
+        }
+    }
+    
+    private func configureEmailComposeVC() -> MFMailComposeViewController {
+        let emailComposeVC = MFMailComposeViewController()
+        emailComposeVC.mailComposeDelegate = self
+        
+        emailComposeVC.setToRecipients(["contact@lazyself.io"])
+        emailComposeVC.setSubject("About Goet App.")
+        emailComposeVC.setMessageBody("", isHTML: false)
+        
+        return emailComposeVC
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    private func showSendEmailErrorAlert() {
+        let alert = UIAlertController(
+            title: "Email cannot be sent.",
+            message: "Sorry, email cannot be sent for now, please try at a later time.",
+            actions: [.ok]
+        )
+        present(alert, animated: false, completion: nil)
+    }
 
     func getText(title: String, text: String) {
         textTitle = title
